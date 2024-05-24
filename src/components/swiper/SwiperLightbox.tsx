a81aac1ce636206ac850b0825eaf8db5233d96b9
ridd <riddma@qq.com>
2024-05-25 02:41:34 +0800

    Feat: Exif display prototype and looping removed.

--- src/components/swiper/SwiperLightbox.tsx
+++ src/components/swiper/SwiperLightbox.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import "photoswipe/style.css";
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/pagination";
-import "swiper/css/keyboard";
-import PhotoSwipeLightbox from "photoswipe/lightbox";
-import "photoswipe/style.css";
 
 import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Keyboard } from "swiper/modules";
 import PhotoCard from "@/src/components/PhotoCard";
 import { RdPhoto } from "@/src/util/veliteUtils";
 import { min } from "lodash";
+import { motion, AnimatePresence } from "framer-motion";
+import { transitionApple } from "@/src/styles/framer-motion";
+import { Icon } from "@iconify-icon/react";
 
 const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeight?: number; featured?: boolean }> = ({
   images,
@@ -25,6 +27,7 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
   const [maxWidth, setMaxWidth] = useState<number>(0);
   const [maxH, setMaxH] = useState<number>(0);
   const [isMobile, setIsMobile] = useState<boolean>(true);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   const [showExif, setShowExif] = useState<boolean>(false);
 
   useEffect(() => {
@@ -38,6 +41,7 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
   useEffect(() => {
     if (!galleryRef.current) return;
     let lightbox = new PhotoSwipeLightbox({
+      dataSource: images,
       gallery: galleryRef.current,
       children: ".swiper-slide a",
       loop: true,
@@ -50,18 +54,17 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
       pswpModule: () => import("photoswipe"),
     });
     lightbox.on("contentActivate", ({ content }) => {
+      setActiveIndex(content.index);
+      swiperRef.current!.swiper.slideToLoop(content.index, 0);
       setShowExif(true);
-      // content becomes active (the current slide)
-      // can be default prevented
-      console.log("contentActivate", content);
     });
     lightbox.on("contentResize", ({ content, width, height }) => {
-      // content will be resized
-      // can be default prevented
-      console.log("contentResize", content, width, height);
+      // console.log("contentResize", content, width, height);
+    });
+    lightbox.on("close", () => {
+      setShowExif(false);
     });
     lightbox.on("destroy", () => {
-      setShowExif(false);
       setSwiperAutoplay(autoplay);
     });
     lightbox.init();
@@ -69,7 +72,7 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
     return () => {
       lightbox.destroy();
     };
-  }, [autoplay]);
+  }, []);
 
   const setSwiperAutoplay = (to: boolean) => {
     if (!swiperRef.current) return;
@@ -82,9 +85,54 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
 
   return (
     <div ref={galleryRef}>
-      <div className="card fixed top-1/2 right-0">
-        <pre>{JSON.stringify(images, null, 2)}</pre>
-      </div>
+      <AnimatePresence>
+        {showExif && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={transitionApple}
+            className="card fixed top-[60px] right-0 max-w-[300px] max-h-[calc(100vh-120px)] z-[999999] p-4 flex flex-col gap-4"
+          >
+            {/*<pre>{JSON.stringify(images[activeIndex].exif, null, 2)}</pre>*/}
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="formkit:datetime" />
+              {images[activeIndex].exif.exif?.DateTimeOriginal?.description ?? "未知拍摄时间"}
+              {images[activeIndex].exif.exif?.OffsetTimeOriginal?.description ?? "未知时区"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="fluent:slide-size-24-regular" />
+              {images[activeIndex].width} x {images[activeIndex].height}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="heroicons:camera" />
+              {images[activeIndex].exif.exif?.Make?.description ?? "未知制造商"}
+              {`, `}
+              {images[activeIndex].exif.exif?.Model?.description ?? "未知型号"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="bxs:cylinder" />
+              {images[activeIndex].exif.exif?.LensModel?.description ?? "未知镜头"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="system-uicons:ruler" />
+              {images[activeIndex].exif.exif?.FocalLength?.description ?? "未知焦距"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="material-symbols:shutter-speed-rounded" />
+              {`${images[activeIndex].exif.exif?.ExposureTime?.description} s` ?? "未知曝光时间"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="carbon:aperture" />
+              {images[activeIndex].exif.exif?.FNumber?.description ?? "未知光圈"}
+            </div>
+            <div className="flex flex-row gap-2 items-center">
+              <Icon className="text-xl" icon="carbon:iso-filled" />
+              {images[activeIndex].exif.exif?.ISOSpeedRatings?.description ?? "未知 ISO"}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Swiper
         ref={swiperRef}
         className="my-swiper"
@@ -92,11 +140,10 @@ const SwiperLightbox: React.FC<{ images: RdPhoto[]; autoplay?: boolean; maxHeigh
         slidesPerView="auto"
         centeredSlides={isMobile}
         grabCursor={true}
-        autoplay={autoplay ? { delay: 5000, disableOnInteraction: true } : false}
-        keyboard={{ enabled: true, onlyInViewport: true }}
-        loop={true}
+        autoplay={autoplay ? { delay: 5000, disableOnInteraction: true, pauseOnMouseEnter: true } : false}
+        loop={false}
         parallax={true}
-        modules={[Autoplay, Pagination, Keyboard]}
+        modules={[Autoplay, Pagination]}
       >
         {images.map((image, index) => (
           <SwiperSlide key={image.slug} style={{ width: "auto" }}>
